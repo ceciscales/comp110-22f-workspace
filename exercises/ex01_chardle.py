2cc86430f94527c241c457db4df2204c70ab8fdc
Cecilia Scales <scalesc@ad.unc.edu>
2022-08-27 20:53:11 -0400

    Created my first Chardle! Progress on EX01

---
+++ exercises/ex01_chardle.py
@@ -0,0 +1,60 @@
+"""EX01 - Chardle - A cute step toward Wordle"""
+
+
+__author__ = "730396639"
+
+
+wordle_word: str = input("Enter a 5-character word: ")
+
+if len(wordle_word) != 5:
+    exit(print("Error: Word must contain 5 characters "))  
+
+single_character: str = input("Enter a single character: ")
+
+if len(single_character) != 1:
+    exit(print("Character must be a single character. "))  
+
+if single_character == (wordle_word)[0]:
+    print(single_character + " found at index 0 ")
+
+if single_character == (wordle_word)[1]:
+    print(single_character + " found at index 1 ")
+
+if single_character == (wordle_word)[2]:
+    print(single_character + " found at index 2 ")
+
+if single_character == (wordle_word)[3]:
+    print(single_character + " found at index 3 ")   
+
+if single_character == (wordle_word)[4]:
+    print(single_character + " found at index 4 ") 
+
+
+total_count: int = 0
+if single_character == (wordle_word)[0]:
+    total_count = total_count + 1
+
+if single_character == (wordle_word)[1]:
+    total_count = total_count + 1
+
+if single_character == (wordle_word)[2]:
+    total_count = total_count + 1
+
+if single_character == (wordle_word)[3]:
+    total_count = total_count + 1
+
+if single_character == (wordle_word)[4]:
+    total_count = total_count + 1 
+
+
+if int(total_count) == int(0):
+    print(("No instances of " + single_character + " found in " + wordle_word ))
+
+if int(total_count) == int(1):
+    print(("1 instance of " + single_character + " found in " + wordle_word ))
+
+if int(total_count) >= int(2):
+    print((str(total_count)) + " instances of " + single_character + " found in " + wordle_word )
+
+
+
