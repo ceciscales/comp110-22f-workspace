1bc8db1512107b18acd54f4ba5bb8071e2f0be35
Cecilia Scales <scalesc@ad.unc.edu>
2022-09-06 18:56:21 -0400

    taking out exits

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -1,22 +1,21 @@
 """EX02 - One-Shot Wordle!"""
 
+__author__ = "730396639"
 
-__author__: str= "730396639"
-
-# Creating the wordle
+ # Creating the wordle
 secret_word: str= "python"
 
-# Making sure that the guess has sufficient characters
+ # Making sure that the guess has sufficient characters
 guess: str= input(f"What is your 6-letter guess? ")
 while len(guess) != len(secret_word):
     guess = input(f"That was not 6 letters! Try again: ")
 
-# Labelling variables for each box color
+ # Labelling variables for each box color
 WHITE_BOX: str = "\U00002B1C"
 GREEN_BOX: str = "\U0001F7E9"
 YELLOW_BOX: str = "\U0001F7E8"
 
-# Letter 1
+ # Letter 1
 if guess[0] == (secret_word)[0]:
    a=(GREEN_BOX)
 elif guess[0] == (secret_word)[1]:
@@ -31,7 +30,7 @@ elif guess[0] == (secret_word)[5]:
     a= (YELLOW_BOX)
 else: a= (WHITE_BOX)
 
-# Letter 2
+ # Letter 2
 if guess[1] == (secret_word)[1]:
     b=(GREEN_BOX)
 elif guess[1] == (secret_word)[0]:
@@ -46,7 +45,7 @@ elif guess[1] == (secret_word)[5]:
     b= (YELLOW_BOX)
 else: b=(WHITE_BOX)
 
-# Letter 3
+ # Letter 3
 if guess[2] == (secret_word)[2]:
     c=(GREEN_BOX)
 elif guess[2] == (secret_word)[0]:
@@ -61,7 +60,7 @@ elif guess[2] == (secret_word)[5]:
     c= (YELLOW_BOX)
 else: c=(WHITE_BOX)
 
-# Letter 4
+ # Letter 4
 if guess[3] == (secret_word)[3]:
     d=(GREEN_BOX)
 elif guess[3] == (secret_word)[0]:
@@ -76,7 +75,7 @@ elif guess[3] == (secret_word)[5]:
     d= (YELLOW_BOX)
 else: d=(WHITE_BOX)
 
-#Letter 5
+ # Letter 5
 if guess[4] == (secret_word)[4]:
     e=(GREEN_BOX)
 elif guess[4] == (secret_word)[0]:
@@ -91,7 +90,7 @@ elif guess[4] == (secret_word)[5]:
     e= (YELLOW_BOX)
 else: e=(WHITE_BOX)
 
-# Letter 6
+ # Letter 6
 if guess[5] == (secret_word)[5]:
     f=(GREEN_BOX)
 elif guess[5] == (secret_word)[0]:
@@ -106,13 +105,13 @@ elif guess[5] == (secret_word)[4]:
     f= (YELLOW_BOX)
 else: f=(WHITE_BOX)
 
-# Printing the wordle boxes
+ # Printing the wordle boxes
 print(f"{a}{b}{c}{d}{e}{f}")
 
 
-# Did you get the secret word right?
+ # Did you get the secret word right?
 while len(guess) == len(secret_word):
     if guess == secret_word:
-        exit(print(f"Woo! You got it! "))
+        (print(f"Woo! You got it! "))
     if guess != secret_word:
-        exit(f"Not quite. Play again soon! ")
+        (f"Not quite. Play again soon! ")
